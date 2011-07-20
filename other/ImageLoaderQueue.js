86f06d9c518135f7e3c646c68bb7848ed691c525
millermedeiros <miller@millermedeiros.com>
2011-07-20 11:54:05 -0300

    retab

--- other/ImageLoaderQueue.js
+++ other/ImageLoaderQueue.js
@@ -1,57 +1,57 @@
 define(function(){
-	
-	/**
-	 * very basic image loader queue
-	 * - if you are using RequireJS favor require/image plugin.
-	 * @author Miller Medeiros
-	 * @version 0.0.3 (2011/07/11)
-	 */
+    
+    /**
+     * very basic image loader queue
+     * - if you are using RequireJS favor require/image plugin.
+     * @author Miller Medeiros
+     * @version 0.0.3 (2011/07/11)
+     */
     function ImageLoaderQueue(items){
-		this._items = items;
-	}
-	
-	ImageLoaderQueue.prototype = {
-		
-		_numLoaded : 0,
+        this._items = items;
+    }
+    
+    ImageLoaderQueue.prototype = {
+        
+        _numLoaded : 0,
 
         _cache : {},
-		
-		isComplete : function(){
-			return this._numLoaded >= this._items.length;
-		},
-		
-		loadAll : function(callback){
-			var i = 0,
-				img,
-				numTotal = this._items.length,
-				hasCallback = (typeof callback === 'function'),
-				self = this,
+        
+        isComplete : function(){
+            return this._numLoaded >= this._items.length;
+        },
+        
+        loadAll : function(callback){
+            var i = 0,
+                img,
+                numTotal = this._items.length,
+                hasCallback = (typeof callback === 'function'),
+                self = this,
                 src;
-			
-			if(hasCallback && this.isComplete()){
-				callback();
-				return;
-			}
-			
-			function registerLoad(evt){
-				delete evt.target.onload;
-				delete evt.target.onerror;
-				self._numLoaded += 1;
-				if(self.isComplete()){
-					callback();
-				}
+            
+            if(hasCallback && this.isComplete()){
+                callback();
+                return;
             }
-			
-			for(; i < numTotal; i++){
-				img = new Image();
+            
+            function registerLoad(evt){
+                delete evt.target.onload;
+                delete evt.target.onerror;
+                self._numLoaded += 1;
+                if(self.isComplete()){
+                    callback();
+                }
+            }
+            
+            for(; i < numTotal; i++){
+                img = new Image();
                 src = this._items[i];
-				if(hasCallback){
-					img.onload = img.onerror = registerLoad;
-				}
-				img.src = src;
+                if(hasCallback){
+                    img.onload = img.onerror = registerLoad;
+                }
+                img.src = src;
                 self._cache[src] = img;
-			}
-		},
+            }
+        },
 
         getImg : function(src){
             return self._cache[src];
@@ -61,8 +61,8 @@ define(function(){
             delete this._cache;
             delete this._items;
         }
-		
-	};
-	
-	return ImageLoaderQueue;
+        
+    };
+    
+    return ImageLoaderQueue;
 });
