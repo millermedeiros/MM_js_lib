7ae822cc5b8dce0b9429d5cccd4ae08237e35c1b
millermedeiros <miller@millermedeiros.com>
2011-08-19 19:10:32 -0300

    update timeUtils and add unit tests for it

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -1,16 +1,17 @@
-/**
- * Utilities for time manipulation
- * @author Miller Medeiros
- * @version 0.0.2 (2011/04/05)
- * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
- */
 define(function(){
 	
 	function part(ms, unit, mod){
-		return Math.floor(ms/unit) % mod;
+		return (Math.floor(ms/unit) % mod) ^ 0;
 	}
-	
+
+    /**
+     * Utilities for time manipulation
+     * @exports mm/utils/timeUtils
+     * @version 0.1.0 (2011/08/19)
+     * @author Miller Medeiros
+     */
 	var timeUtils = {
+
 		//aproximated miliseconds time conversion (using a 365.242199 days year)
 		MILISECOND : 1,
 		SECOND : 1000,
@@ -21,15 +22,33 @@ define(function(){
 		MONTH : 2629743830,
 		YEAR : 31556926000,
 		
+        /**
+         * Parse timestamp into an object.
+         * IMPORTANT: it doesn't handle Weeks, Months and Years. 
+         * @param {number} ms   Miliseconds
+         * @return {{miliseconds:number, seconds:number, minutes:number, hours:number, days:number)}
+         */
 		split : function(ms){
-			var time = {};
-			time.miliseconds = part(ms, this.MILISECOND, 1000);
-			time.seconds = part(ms, this.SECOND, 60);
-			time.minutes = part(ms, this.MINUTE, 60);
-			time.hours = part(ms, this.HOUR, 24);
-			time.days = part(ms, this.DAY, 365);
+			var time = {
+                miliseconds : part(ms, timeUtils.MILISECOND, 1000),
+                seconds : part(ms, timeUtils.SECOND, 60),
+                minutes : part(ms, timeUtils.MINUTE, 60),
+                hours : part(ms, timeUtils.HOUR, 24),
+                days : Math.floor(ms / timeUtils.DAY)
+            };
 			return time;
-		}
+		},
+
+        /**
+        * Format timestamp into a time string.
+        * @param {number} ms Miliseconds
+        * @return {string} Time string on the format 'HH:MM:SS'.
+        */
+        toTimeString : function(ms){
+            var output = Math.floor(ms / timeUtils.HOUR) +':'+ part(ms, timeUtils.MINUTE, 60) +':'+ part(ms, timeUtils.SECOND, 60);
+            return output.replace(/(:|^)([0-9])(?=:|$)/g, '$10$2'); //add zero before single digits
+        }
+        
 	};
 	
 	return timeUtils;
