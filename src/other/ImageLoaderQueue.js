46118cedceb283501322add706ba061e7d758a9f
millermedeiros <miller@millermedeiros.com>
2012-12-20 00:35:54 -0200

    update ImageLoaderQueue to latest

--- src/other/ImageLoaderQueue.js
+++ src/other/ImageLoaderQueue.js
@@ -1,79 +1,110 @@
-define(function(){
+define(
+    [
+       'signals',
+       'amd-utils/lang/toArray',
+       'amd-utils/array/append'
+    ],
+    function(Signal, toArray, append){
 
-    var _noop = function(){};
+        var _noop = function(){};
 
-    /**
-     * very basic image loader queue
-     * @author Miller Medeiros
-     * @version 0.0.5 (2012/01/04)
-     */
-    function ImageLoaderQueue(items){
-        this._items = items;
-    }
-
-    ImageLoaderQueue.prototype = {
-
-        _numLoaded : 0,
-
-        _cache : {},
-
-        isComplete : function(){
-            return this._numLoaded >= this._items.length;
-        },
-
-        loadAll : function(callback){
-            var i = 0,
-                img,
-                numTotal = this._items.length,
-                hasCallback = (typeof callback === 'function'),
-                self = this,
-                src;
-
-            if(hasCallback && this.isComplete()){
-                callback();
-                return;
-            }
-
-            function registerLoad(evt){
-                evt = evt || window.event; //IE fix
-                var img = evt.target || evt.srcElement; //IE fix
-                if(img){
-                    // try/catch for IE7
-                    try {
-                        delete img.onload;
-                        delete img.onerror;
-                    } catch(err) {
-                        img.onload = _noop;
-                        img.onerror = _noop;
-                    }
-                }
-                self._numLoaded += 1;
-                if(self.isComplete()){
-                    callback();
-                }
-            }
-
-            for(; i < numTotal; i++){
-                img = new Image();
-                src = this._items[i];
-                if(hasCallback){
-                    img.onload = img.onerror = registerLoad;
-                }
-                img.src = src;
-                self._cache[src] = img;
-            }
-        },
-
-        getImg : function(src){
-            return self._cache[src];
-        },
-
-        dispose : function(){
-            delete this._cache;
-            delete this._items;
+        /**
+         * very basic image loader queue
+         * @author Miller Medeiros
+         * @version 0.2.0 (2012/09/25)
+         */
+        function ImageLoaderQueue(){
+            this._items = [];
+            this._cache = {};
+            this.on = {
+                complete : new Signal(),
+                progress : new Signal(),
+                error : new Signal()
+            };
+            this.on.complete.memorize = true;
         }
 
-    };
+        ImageLoaderQueue.prototype = {
 
-    return ImageLoaderQueue;
-});
+            add : function(var_args){
+                var items;
+                if (arguments.length === 1 && typeof arguments[0] !== 'string') {
+                    // if array-like add multiple items
+                    items = toArray( arguments[0] );
+                } else {
+                    // if strings push to array
+                    items = toArray(arguments);
+                }
+                append(this._items, items);
+            },
+
+            _numLoaded : 0,
+
+            isComplete : function(){
+                return this._numLoaded >= this._items.length;
+            },
+
+            loadAll : function(){
+                var i, img,
+                    numTotal = this._items.length,
+                    self = this,
+                    src;
+
+                if ( this.isComplete() ) {
+                    return;
+                }
+
+                function registerLoad(evt){
+                    evt = evt || window.event; //IE fix
+                    var img = evt.target || evt.srcElement; //IE fix
+                    if(img){
+                        // try/catch for IE7
+                        try {
+                            delete img.onload;
+                            delete img.onerror;
+                        } catch(err) {
+                            img.onload = _noop;
+                            img.onerror = _noop;
+                        }
+                    }
+                    if (evt.type === 'error') {
+                        self.on.error.dispatch(self, evt);
+                    } else {
+                        self.on.progress.dispatch(self, evt);
+                    }
+                    self._numLoaded += 1;
+                    if(self.isComplete()){
+                        self.on.complete.dispatch(self);
+                    }
+                }
+
+                for(i = 0; i < numTotal; i++){
+                    src = this._items[i];
+                    // request same image only once
+                    if ( !(src in self._cache) ) {
+                        img = new Image();
+                        img.onload = img.onerror = registerLoad;
+                        img.src = src;
+                        self._cache[src] = img;
+                    }
+                }
+            },
+
+            getProgress : function(){
+                return this._numLoaded / this._items.length;
+            },
+
+            getImg : function(src){
+                return this._cache[src];
+            },
+
+            dispose : function(){
+                delete this._cache;
+                delete this._items;
+            }
+
+        };
+
+        return ImageLoaderQueue;
+    }
+);
