5bcdf7b3c3e70c1556f64bd1afe09c3192e70f0b
millermedeiros <miller@millermedeiros.com>
2013-08-23 16:55:55 -0300

    improve ScrollPane performance, specially on Android.

--- src/other/ScrollPane.js
+++ src/other/ScrollPane.js
@@ -20,11 +20,10 @@ define([
         /**
          * Scroll Pane - scrollable touch container
          * it will use CSS transforms if available or fallback to jQuery
-         * @version 0.3.2 (2012/04/15)
+         * @version 0.3.3 (2013/08/23)
          * @author Miller Medeiros
          */
         function ScrollPane(frameElm, contentHolder, numPanes){
-
             this._paneWidth = frameElm.offsetWidth;
             this._frame = frameElm;
             this._content = contentHolder;
@@ -32,6 +31,8 @@ define([
                 this.$_content = $(contentHolder); //cache jquery object..
             }
             this.numPanes = numPanes;
+            this._maxX = this._paneWidth / 2;
+            this._minX = (this.numPanes - 1) * -this._paneWidth - this._maxX;
             this._touchController = new TouchController(this._frame);
             this._setup();
             this.updated = new signals.Signal();
@@ -59,34 +60,48 @@ define([
 
                 this._frame.style.cssText += '; overflow:hidden; position:relative; '+ (userSelect? userSelect +':none;' : '');
                 //preserve-3d enables hardware acceleration, pointer-events:none to avoid retain of click event by child elements. position-absolute in case not using transforms
-                this._content.style.cssText += '; '+ (transformStyle? transformStyle +':preserve-3d;' : '') +' width:'+ Math.ceil(this.numPanes * this._paneWidth) +'px; pointer-events:none; position:absolute;';
+                this._content.style.cssText += '; '+ (transformStyle? transformStyle +':preserve-3d;'+ _transformProperty +':translate3d(0,0,0);' : '') +' width:'+ Math.ceil(this.numPanes * this._paneWidth) +'px; pointer-events:none; position:absolute;';
                 tc.touchStarted.add(this._touchStartHandler, this);
-                tc.touchMoved.add(this._touchMoveHandler, this);
-                tc.touchEnded.add(this._touchEndHandler, this);
             },
 
             _touchStartHandler : function(evt, startPos){
                 var x;
                 if(_supportCSSTransition){
-                    x = this._content.style[_transformProperty].replace(/(translateX\()(-?[0-9]+)(px\))/, '$2');
+                    x = this._content.style[_transformProperty].replace(/(translate3d\(\s*)(-?[0-9]+)((?:px)?.+)/, '$2');
                 } else {
                     x = this._content.style.left;
                 }
                 this._touchStartX = x? parseInt(x, 10) : 0; //parseInt('') === NaN
                 this.prevPane = this.currentPane;
+
+                this.lockScroll = false;
+                this._touchController.touchMoved.add(this._touchMoveHandler, this);
             },
 
             _touchMoveHandler : function(evt, changePos){
                 var cx = changePos.x,
-                    friction;
+                    friction = 1;
 
                 if ((this.currentPane === 0 && cx < this._paneWidth) || (this.currentPane === this.numPanes - 1 && cx > -this._paneWidth)) {
-                    friction = ((this.currentPane === 0 && cx > 0) || (this.currentPane === this.numPanes - 1 && cx < 0))? 1 - (Math.abs(cx)/this._paneWidth) / 2 : 1;
-                    this._slideTo(this._touchStartX + cx * friction, false, 0);
+                    friction = ((this.currentPane === 0 && cx > 0) || (this.currentPane === this.numPanes - 1 && cx < 0))? 1 - (Math.max(Math.abs(cx),this._paneWidth) / this._paneWidth) / 4 : 1;
+                }
+
+                if (!this.lockScroll){
+                    // this should only be triggered at the first move
+                    if (Math.abs(changePos.y) < Math.abs(changePos.x)) {
+                        this.lockScroll = true;
+                    } else {
+                        this._touchController.touchMoved.remove(this._touchMoveHandler, this);
+                        this._touchController.touchEnded.remove(this._touchEndHandler, this);
+                        this.lockScroll = false;
+                        return;
+                    }
                 }
 
                 if(this.lockScroll){
                     evt.preventDefault();
+                    this._slideTo(this._touchStartX + cx * friction, false, 0);
+                    this._touchController.touchEnded.add(this._touchEndHandler, this);
                 }
             },
 
@@ -101,6 +116,8 @@ define([
                     inc = (cx < 0)? 1 : -1;
                 }
                 this.goTo(this.currentPane + inc);
+                this.lockScroll = false;
+                this._touchController.touchEnded.remove(this._touchEndHandler, this);
             },
 
             goTo : function(index){
@@ -129,12 +146,13 @@ define([
 
             _slideTo : function(destX, updateCurrent, duration){
                 duration = duration || 0;
+                destX = clamp(destX, this._minX, this._maxX);
 
                 var dest = destX +'px';
 
                 if(_supportCSSTransition){
                     this._content.style[_transitionProperty] = vendorPrefix.css('transform') +' '+ duration +'ms ease-out';
-                    this._content.style[_transformProperty] = 'translateX('+ dest +')';
+                    this._content.style[_transformProperty] = 'translate3d('+ dest +', 0, 0)';
                 } else if(duration && $) {
                     //fallback to jquery only for animation
                     this.$_content.animate({'left': dest}, duration, _jqEasing);
